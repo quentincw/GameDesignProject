55f287bba76903f2aa75e6d8cadb84a66f2dd6c0
sjramotowski <sjramotowski@email.wm.edu>
2025-03-09 18:55:20 -0400

    fixed indentation

--- bin/testEnemies.cpp
+++ bin/testEnemies.cpp
@@ -54,115 +54,113 @@ int main(int argc, char** argv)
 
     /*** Initialization ***/
   
-  // Initialize SDL
-  if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) csci437_error("SDL could not initialize!");
+    // Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) csci437_error("SDL could not initialize!");
 
-  // Create window
-  SDL_Window* window = SDL_CreateWindow( "CSCI-437 Skeleton", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-  if( window == NULL ) csci437_error("Window could not be created!");
+    // Create window
+    SDL_Window* window = SDL_CreateWindow( "CSCI-437 Skeleton", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == NULL ) csci437_error("Window could not be created!");
 
-  // Small delay to allow the system to create the window.
-  SDL_Delay(100);
+    // Small delay to allow the system to create the window.
+    SDL_Delay(100);
     
-  // Create renderer
-  SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
-  if (renderer == NULL) csci437_error("Unable to create renderer!");
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+    if (renderer == NULL) csci437_error("Unable to create renderer!");
 
-  vector<GameProcess*> objectList;
+    vector<GameProcess*> objectList;
   
-  Roach roach1(1000, 700);
+    Roach roach1(1000, 700);
 
-  vector<string> tags = roach1.getTags();
-  for(int i = 0; i < tags.size(); i++){
-    cout << tags[i];
-}
+    vector<string> tags = roach1.getTags();
+    for(int i = 0; i < tags.size(); i++){
+        cout << tags[i];
+    }
 
-  PlayerProjectile ball1(0, 0, 3.0f, 3.0f);
+    PlayerProjectile ball1(0, 0, 3.0f, 3.0f);
 
-  Spitter spitter1(200,500);
+    Spitter spitter1(200,500);
 
-  objectList.push_back(&roach1);
-  objectList.push_back(&ball1);
-  objectList.push_back(&spitter1);
-
-
+    objectList.push_back(&roach1);
+    objectList.push_back(&ball1);
+    objectList.push_back(&spitter1);
 
 
-  /*** Main Loop ***/
-  bool running = true;
-  SDL_Event e;
-  const int FPS = 60;
-  const int TARGETMS = 1000/FPS;
 
-  // While application is running
-  while( running )
-  {
-  
-    // get start timee
-    int startMS = SDL_GetTicks();
-    // Handle events on queue
-    while( SDL_PollEvent( &e ) != 0)
+
+    /*** Main Loop ***/
+    bool running = true;
+    SDL_Event e;
+    const int FPS = 60;
+    const int TARGETMS = 1000/FPS;
+
+    // While application is running
+    while( running )
     {
-      // User requests quit
-      if( e.type == SDL_QUIT ) running = false;
+  
+        // get start timee
+        int startMS = SDL_GetTicks();
+        // Handle events on queue
+        while( SDL_PollEvent( &e ) != 0)
+        {
+              // User requests quit
+            if( e.type == SDL_QUIT ) running = false;
 
-      // User presses a key
-      if( e.type == SDL_KEYDOWN ){
+            // User presses a key
+            if( e.type == SDL_KEYDOWN ){
 
-        switch(e.key.keysym.sym){
-            case SDLK_q:
-                running = false;
-                break;
-            /*case SDLK_UP:
-                roach1.UpdateAI(roach1.getXpos(), 100);
-                break;
-            case SDLK_DOWN:
-                roach1.UpdateAI(roach1.getXpos(), SCREEN_HEIGHT - 100);
-                break;
-            case SDLK_RIGHT:
-                roach1.UpdateAI(SCREEN_WIDTH - 100, roach1.getYpos());
-                break;
-            case SDLK_LEFT:
-                roach1.UpdateAI(100, roach1.getYpos());
-                break;*/
+                switch(e.key.keysym.sym){
+                    case SDLK_q:
+                        running = false;
+                        break;
+                    /*case SDLK_UP:
+                        roach1.UpdateAI(roach1.getXpos(), 100);
+                        break;
+                    case SDLK_DOWN:
+                        roach1.UpdateAI(roach1.getXpos(), SCREEN_HEIGHT - 100);
+                        break;
+                    case SDLK_RIGHT:
+                        roach1.UpdateAI(SCREEN_WIDTH - 100, roach1.getYpos());
+                        break;
+                    case SDLK_LEFT:
+                        roach1.UpdateAI(100, roach1.getYpos());
+                        break;*/
+                }
+            }
+            else {
+            // no keys pressed,
+            }
         }
 
-      }
-      else{
-        // no keys pressed,
-
-      }
-    }
-
-    for(int i = 0; i < objectList.size(); i++){
-        objectList[i]->Update(1);
-    }
-    //roach1.UpdateAI(ball1.getXpos(), ball1.getYpos());
-    roach1.UpdateAI(ball1.getHitbox());
-    spitter1.UpdateAI(ball1.getHitbox());
+        for(int i = 0; i < objectList.size(); i++){
+            objectList[i]->Update(1);
+        }
+        //roach1.UpdateAI(ball1.getXpos(), ball1.getYpos());
+        roach1.UpdateAI(ball1.getHitbox());
+        spitter1.UpdateAI(ball1.getHitbox());
     
-    if(spitter1.hasChildren()){
-        objectList.push_back(spitter1.getChildren());
-    }
+        if(spitter1.hasChildren()){
+            objectList.push_back(spitter1.getChildren());
+        }
         
 
-    projectileCollision(&ball1);
+        projectileCollision(&ball1);
 
-    // draw screen
-    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-    SDL_RenderClear( renderer );
-    for(int i = 0; i < objectList.size(); i++){
-        objectList[i]->Render( renderer );
-    }
-    SDL_RenderPresent( renderer );
+        // draw screen
+        SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+        SDL_RenderClear( renderer );
+        for(int i = 0; i < objectList.size(); i++){
+            objectList[i]->Render( renderer );
+        }
+        SDL_RenderPresent( renderer );
 
-    // delta time calculation
-    int deltaMS = SDL_GetTicks() - startMS;
-    if(deltaMS < TARGETMS){
-        SDL_Delay(TARGETMS - deltaMS);
+        // delta time calculation
+        int deltaMS = SDL_GetTicks() - startMS;
+        if(deltaMS < TARGETMS){
+            SDL_Delay(TARGETMS - deltaMS);
+        }
     }
-  }
   
-  // Done.
-  return 0;
+    // Done.
+    return 0;
 }
