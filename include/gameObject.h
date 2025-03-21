cded403b004658630cd41d40b6f95a8476902bde
sjramotowski <sjramotowski@email.wm.edu>
2025-03-21 14:27:43 -0400

    pushed gameObject

--- include/gameObject.h
+++ include/gameObject.h
@@ -1,52 +1,15 @@
-#ifndef GOBJECT_H
-#define GOBJECT_H
+#ifndef _GAMEOBJECT_H_
+#define _GAMEOBJECT_H_
 
 #include <stdlib.h>
 #include <SDL.h>
 #include <SDL2_gfxPrimitives.h>
-#include <vector>
-#include <string>
 
-struct Rectangle {
-    int x;
-    int y;
-    int width;
-    int height;
-};
-
-struct Point {
-    int x;
-    int y;
+class GameObject{
+	public:
+		virtual void Update(float deltaTime) = 0;
+		virtual void Render(SDL_Renderer* renderer) = 0;
 };
 
 
-class GameObject {
-
-    public:
-
-        // constructor
-        GameObject();
-
-        // draws the object
-        virtual void Render(SDL_Renderer* renderer) = 0;
-
-        // get the hitbox of the game object
-        Rectangle getHitbox() const;
-
-        // sets the position of the rectangle
-        void setPosition(int x, int y);
-
-        // calculates and returns the center point of the hitbox
-        Point getCenter(Rectangle* rectangle) const;
-
-    
-    protected:
-
-        // hitbox for game object
-        Rectangle hitbox;
-
-        
-};
-
-
-#endif
+#endif /* _GAMEOBJECT_H_ */
